1a7c039aca4675d250c6cbf7193073ca4cc60989
justin rebbeck <jrebbeck2bsd220.org>
2024-03-04 09:48:53 -0600

    Initial Commit

---
+++ API List/API_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_ListApp.swift
+//  API List
+//
+//  Created by Justin Rebbeck on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct API_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
